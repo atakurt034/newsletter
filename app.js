433091ceb417b06463a18cdac01ade2eaa4c532d
atakurt034 <69969985+atakurt034@users.noreply.github.com>
2020-12-23 01:45:05 +0800

    fixed api key and added module

--- app.js
+++ app.js
@@ -1,63 +1,72 @@
 // jshint esversion:6
 // jshint esversion:8
-const express =  require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const request = require("request");
-const https = require("https");
-const mailchimp = require('@mailchimp/mailchimp_marketing');
+import express from 'express'
+import bodyParser from 'body-parser'
+import request from 'request'
+import https from 'https'
+import mailchimp from '@mailchimp/mailchimp_marketing'
+import dotenv from 'dotenv'
+import path from 'path'
 
+const app = express()
+const __dirname = path.resolve()
+dotenv.config()
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/signup.html");
-});
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/signup.html')
+})
 
-app.post("/", (req, res)=> {
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const email = req.body.email;
+app.post('/', (req, res) => {
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const email = req.body.email
 
+  mailchimp.setConfig({
+    apiKey: process.env.API_KEY,
+    server: process.env.SERVER_KEY,
+  })
 
-    mailchimp.setConfig({
-      apiKey: "5d43b0711a180a039dca85adc9a98cbb-us2",
-      server: "us2",
-    });
-    
-    const run = async () => {
-      const response = await mailchimp.lists.batchListMembers("5319835474", {
-        members: [{
+  const run = async () => {
+    const response = await mailchimp.lists
+      .batchListMembers('5319835474', {
+        members: [
+          {
             email_address: email,
-            status: "subscribed",
+            status: 'subscribed',
             merge_fields: {
-                FNAME: firstName,
-                LNAME: lastName
-            }
-        }],
-      }).then((response) => {
-          res.sendFile(__dirname + "/success.html");
-        console.log(
+              FNAME: firstName,
+              LNAME: lastName,
+            },
+          },
+        ],
+      })
+      .then((response) => {
+        if (response) {
+          if (response.errors.length !== 0) {
+            res.sendFile(__dirname + '/failure.html')
+          } else {
+            res.sendFile(__dirname + '/success.html')
+          }
+          console.log(
             `Successfully added contact as an audience member. 
-            The contact's id is ${response.id}.`
-        );
-    }).catch((err) =>{ 
-        res.sendFile(__dirname + "/failure.html");
-        console.log(err);
-    });
-    
-     
-    
-    
-    
-    
-    
-    
-};
-run();
-
-});
-app.listen(process.env.PORT || 3000, (req, res)=> {
-console.log("server running:3000");
-});
+            The contact's id is ${
+              response.new_members.length > 0
+                ? response.new_members[0].id
+                : 'already registered'
+            }.`
+          )
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+        res.sendFile(__dirname + '/failure.html')
+      })
+  }
+  run()
+})
+app.listen(process.env.PORT || 3000, (req, res) => {
+  console.log('server running:3000')
+})
